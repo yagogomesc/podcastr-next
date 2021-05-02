374b451ca0a09d7fc1c1af6e3658cf81e3289eee
Yago Gomes <yagogomesc@gmail.com>
2021-05-02 10:25:07 -0300

    Adjust date format

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,10 @@
 import format from "date-fns/format";
+import { ptBR } from "date-fns/locale";
 
 import styles from "./styles.module.scss";
 
 export function Header() {
-  const currentDate = format(new Date(), "EEEEEE, d MMMM");
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", { locale: ptBR });
 
   return (
     <header className={styles.headerContainer}>
