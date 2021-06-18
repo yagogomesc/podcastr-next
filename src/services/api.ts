7165f43c79216267c54e71996c934985d3e55323
Yago Gomes <yagogomesc@gmail.com>
2021-06-18 20:10:24 -0300

    include headers direct on api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://api.spotify.com/v1/shows/",
+  baseURL: "https://api.spotify.com/v1/",
+  headers: {
+    Accept: "application/json",
+    Authorization:
+      "Bearer BQDf0QQ7oxW1BgntZxLvkf-O_sffVslhWw6SANtqcRGkOrj4X4sSE5S9AsQXI-NYbC_iCEr4rQUQNqwwqKrj7O_LrcsEo1soHaqfhZuX6DcGSUeXlBjAUlES7647u2b4hmYpnY_ZTG-RGk6uLw",
+  },
 });
